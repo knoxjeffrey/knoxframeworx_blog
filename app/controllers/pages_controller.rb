79fcf86240c1cd3dd2c020ae0251644972f63e55
Jeff Knox <knoxjeffrey@outlook.com>
2015-04-10 11:29:58 +0100

    small addition to PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
-class PagesController
+class PagesController < Knoxframeworx::Controller
   
   def about
-    "I am Jeff"
+    "I'm Jeff and here's a crap load of env info:" + 
+    "\n<pre>\n#{env}\n</pre>" +
+    "This is what Rails has to work with.  " +
+    "For example I can do cool things like handle get, post requests differently by examining REQUEST_METHOD"
   end
   
 end
